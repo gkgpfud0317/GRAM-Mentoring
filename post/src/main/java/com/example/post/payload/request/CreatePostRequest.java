d0f0713d64e6e7dc98fa44a2ba5485937ea558b2
하혜령 <gkgpfud1004@gmail.com>
2021-09-07 19:47:54 +0900

    [ADD] CreatePostRequest

---
+++ post/src/main/java/com/example/post/payload/request/CreatePostRequest.java
@@ -0,0 +1,13 @@
+package com.example.post.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePostRequest {
+    private String title;
+    private String content;
+}
