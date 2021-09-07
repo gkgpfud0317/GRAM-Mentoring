4056413adabb55a9fa8db269c067c8b36bf05b57
하혜령 <gkgpfud1004@gmail.com>
2021-09-07 19:47:22 +0900

    [ADD] repository_Post

---
+++ post/src/main/java/com/example/post/repository/Post.java
@@ -0,0 +1,24 @@
+package com.example.post.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+}
