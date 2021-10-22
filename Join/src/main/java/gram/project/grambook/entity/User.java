766e3cf071330a27cd8c854a6b4d16d22eb1510b
하혜령 <gkgpfud1004@gmail.com>
2021-10-22 16:37:00 +0900

    [FIX] User

--- Join/src/main/java/gram/project/grambook/entity/User.java
+++ Join/src/main/java/gram/project/grambook/entity/User.java
@@ -14,12 +14,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String userId;
+
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
     private String password;
-
-    @Column(nullable = false, unique = true)
-    private String userId;
 }
