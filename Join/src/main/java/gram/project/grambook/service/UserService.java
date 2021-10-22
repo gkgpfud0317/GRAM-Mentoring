6a02be2689dcd47819dff77194dd733a7edf0eaf
하혜령 <gkgpfud1004@gmail.com>
2021-10-22 16:37:35 +0900

    [FIX] UserService

--- Join/src/main/java/gram/project/grambook/service/UserService.java
+++ Join/src/main/java/gram/project/grambook/service/UserService.java
@@ -4,31 +4,40 @@ import gram.project.grambook.dto.JoinDto;
 import gram.project.grambook.entity.User;
 import gram.project.grambook.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public boolean cheakuserIdDuplicate(String userId) {
-        return userRepository.existsByuserID(userId);
+    public ResponseEntity<String> cheakuserIdDuplicate(String userId) {
+
+        userRepository.existsByuserID(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body("SUCCESS");
     }
 
-    public boolean cheaknameDuplicate(String name) {
-        return userRepository.existsByname(name);
+    public ResponseEntity<String> cheaknameDuplicate(String name) {
+
+        userRepository.existsByname(name);
+
+        return ResponseEntity.status(HttpStatus.OK).body("SUCCESS");
     }
 
     public String join(JoinDto joinDto) {
-        User user = User.builder()
+
+        userRepository.save(User.builder()
                 .name(joinDto.getName())
                 .userId(joinDto.getUserId())
                 .password(bCryptPasswordEncoder.encode(joinDto.getPassword()))
-                .build();
-        userRepository.save(user);
+                .build());
+
         return "Success";
     }
 }
