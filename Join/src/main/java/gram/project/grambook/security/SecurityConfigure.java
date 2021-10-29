e68c7e212952e4de205b6033bf7d294f6b72111b
하혜령 <gkgpfud1004@gmail.com>
2021-10-29 20:32:00 +0900

    [FIX] SecurityConfigure

--- Join/src/main/java/gram/project/grambook/security/SecurityConfigure.java
+++ Join/src/main/java/gram/project/grambook/security/SecurityConfigure.java
@@ -1,19 +1,18 @@
 package gram.project.grambook.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
