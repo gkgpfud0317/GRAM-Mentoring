e96d938e42d35baef954726ad7af13f036bd1fd6
하혜령 <gkgpfud1004@gmail.com>
2021-11-11 17:18:45 +0900

    [FIX] AuthController

--- Join/src/main/java/gram/project/grambook/controller/AuthController.java
+++ Join/src/main/java/gram/project/grambook/controller/AuthController.java
@@ -1,8 +1,10 @@
 package gram.project.grambook.controller;
 
 import gram.project.grambook.dto.JoinDto;
+import gram.project.grambook.repository.UserRepository;
 import gram.project.grambook.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    UserRepository userRepository;
     @PostMapping("/join")
     public void generateToken() {
 
