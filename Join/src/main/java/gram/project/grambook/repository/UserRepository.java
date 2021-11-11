f038126c250f029291ecd4509db99fc144a5e547
하혜령 <gkgpfud1004@gmail.com>
2021-11-11 17:19:24 +0900

    [FIX] UserRepository

--- Join/src/main/java/gram/project/grambook/repository/UserRepository.java
+++ Join/src/main/java/gram/project/grambook/repository/UserRepository.java
@@ -3,7 +3,10 @@ package gram.project.grambook.repository;
 import gram.project.grambook.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     boolean existsByName = false;
     boolean existsByUserId = false;
 
