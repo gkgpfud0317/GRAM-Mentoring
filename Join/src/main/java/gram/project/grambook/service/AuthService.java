36713c50699b73e23556daebd55b47d5e3dff5b3
하혜령 <gkgpfud1004@gmail.com>
2022-01-05 15:44:16 +0900

    [FIX] AuthService

--- Join/src/main/java/gram/project/grambook/service/AuthService.java
+++ Join/src/main/java/gram/project/grambook/service/AuthService.java
@@ -1,11 +1,8 @@
 package gram.project.grambook.service;
 
-import gram.project.grambook.dto.JoinDto;
 import gram.project.grambook.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 
 
 @Service
