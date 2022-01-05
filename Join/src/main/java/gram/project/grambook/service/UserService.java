c485ca74f4ef5e4974c44fa9fe531269b0e1bfc6
하혜령 <gkgpfud1004@gmail.com>
2022-01-05 15:43:52 +0900

    [FIX] UserService

--- Join/src/main/java/gram/project/grambook/service/UserService.java
+++ Join/src/main/java/gram/project/grambook/service/UserService.java
@@ -6,7 +6,6 @@ import gram.project.grambook.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
